f4eaed086183773d6855f01e410f50d06a1c28ac
ZVex Dev <zayronvex22@gmail.com>
2025-03-06 15:44:03 +0800

    Update config.js

--- media/settings/config.js
+++ media/settings/config.js
@@ -11,13 +11,13 @@ const version = require("@whiskeysockets/baileys/package.json").version
 // jika menambahkan huruf / text di tutup kurung seperti ini []
 // contoh ['1','2','3']
 // ['1','2','3','4']
-global.botname = "Clairity Bot"
-global.ownerNumber = [`${global.owner}`,'no2','no3'] // no owner yang dapat mengakses all fitur
-global.email = '@gmail.com' // kosongkan jika tidak punya
+global.botname = "ZVex Bot"
+global.ownerNumber = [`${global.owner}`,'6283894131246'] // no owner yang dapat mengakses all fitur
+global.email = 'zayronvex22@gmail.com' // kosongkan jika tidak punya
 global.web = '.com' // kosongkan jika tidak punya
-global.location = 'Banten' // isi daerah mu atau tempat tinggal mu jika tidak mau kosongkan
-global.packname = 'Mayura bot' // wm yg ada di sticker
-global.author = 'dibuat oleh xZiyy' // wm yg ada di sticker
+global.location = 'mars' // isi daerah mu atau tempat tinggal mu jika tidak mau kosongkan
+global.packname = 'Zbot' // wm yg ada di sticker
+global.author = 'dibuat oleh ZVex' // wm yg ada di sticker
 global.imgUrl = "https://files.catbox.moe/8cmckf.jpg" // ubah menggunakan !setimgbot
 global.menuimgUrl = "https://files.catbox.moe/bfwpip.jpg" // ubah menggunakan !setimgmenu
 
